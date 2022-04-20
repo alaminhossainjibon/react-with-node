a0b8b1ff55e6ebb937a83e267386082ebb23014a
alaminhossainjibon <mdalamin7622@gmail.com>
2022-04-20 17:36:05 +0600

    react with node

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, []);
+
+  const handleAdd = event => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email }
+
+    //post data to server
+
+    fetch('http://localhost:5000/user', {
+      method: 'POST', // or 'PUT'
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then(response => response.json())
+      .then(data => {
+        const newUsers = [...users, data];
+        setUsers(newUsers);
+        console.log(data);
+      })
+
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My own data: {users.length} </h1>
+
+      <form onSubmit={handleAdd}>
+        <input type="text" name="name" id="" placeholder='Name' required />
+        <br />
+        <br />
+        <input type="text" name="email" id="" placeholder='Email' required />
+        <br />
+        <br />
+        <input type="submit" value="Ass User" />
+      </form>
+
+
+      <ul>
+        {
+          users.map(user => <li key={user.id}> id: {user.id} name: {user.name} email: {user.email} phone: {user.phone}</li>)
+        }
+      </ul>
+
     </div>
   );
 }
